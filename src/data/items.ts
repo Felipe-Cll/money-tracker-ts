60c16dccdea64deb43e0c737f30c7005f55ba000
Felipe <FelipeCll01@outlook.com>
2022-08-10 15:36:41 -0300

    Primeira versão do Sistema financeiro

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    { date: new Date(2022, 7, 5), category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date(2022, 7, 7), category: 'food', title: 'Burger King', value: 28 },
+    { date: new Date(2022, 7, 16), category: 'rent', title: 'Alguel Apt', value: 2300 },
+    { date: new Date(2022, 10, 18), category: 'salary', title: 'Salário GYM', value: 4500 },
+];
