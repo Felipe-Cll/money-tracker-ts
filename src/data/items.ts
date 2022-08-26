ce49e25b11e3212809fcbcf3bb58ec954e172594
Felipe <FelipeCll01@outlook.com>
2022-08-26 10:32:23 -0300

    Correção bug

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,8 +1,8 @@
 import { Item } from '../types/Item';
 
 export const items: Item[] = [
-    { date: new Date(2022, 7, 5), category: 'salary', title: 'Salário X', value: 4.597 },
-    { date: new Date(2022, 7, 5), category: 'food', title: 'Coffee', value: 35.70 },
+    { date: new Date(2022, 7, 5), category: 'salary', title: 'Salário X', value: 4597 },
+    { date: new Date(2022, 7, 5), category: 'food', title: 'Coffee', value: 25.60 },
     { date: new Date(2022, 7, 7), category: 'pay', title: 'Conta de energia', value: 135 },
     { date: new Date(2022, 7, 15), category: 'extra', title: 'Royalties', value: 2230 },
     { date: new Date(2022, 7, 29), category: 'rent', title: 'Alguel do apartamento', value: 2800 },
